0f6313195963431bbbf2a9c478b532e77b72ac7e
vbekarev <vbekarev@yandex.ru>
2021-09-11 17:06:15 +0700

    Доработан Ping ДТО

--- src/main/java/ru/vbekarev/pong/model/PingDto.java
+++ src/main/java/ru/vbekarev/pong/model/PingDto.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class PingDto implements Serializable {
     private String message;
+    private String counter;
 
     public PingDto() {
     }
@@ -19,4 +20,13 @@ public class PingDto implements Serializable {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    public String getCounter() {
+        return counter;
+    }
+
+    public void setCounter(String counter) {
+        this.counter = counter;
+    }
 }
+
